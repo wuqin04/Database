c739db82ec1b779f22d01e88e3baaa8fd3176ba8
wuqin04 <wuqin419@gmail.com>
2025-08-10 01:09:11 +0800

    created add_data func

--- src/main.py
+++ src/main.py
@@ -9,8 +9,20 @@ def read_data():
     for i in data['clients_data']:
         print(i)
 
+def add_data():
+    client_name = input("Enter client's name: ")
+    client_gmail = input("Enter client's gmail address: ")
+    client_source = input("Enter client's source: ")
+
+    data = dict(name = client_name, gmail = client_gmail, source = client_source)
+
+    write_to_json = json.dumps(data, indent=4)
+    with open('data_storage/data.json', 'w') as file:
+        file.write(write_to_json)
+
 def main():
     read_data()
+    add_data()
 
 if __name__ == "__main__":
     main()
